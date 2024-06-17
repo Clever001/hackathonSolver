8e3e48466fe9e15d60792a7f5c2dc1c1f1ace946
Nikon Vitaly <cakevit@gmail.com>
2024-06-17 10:38:22 +0500

    Изменены xlsx файлы.

--- djangoApi/api/neural/transform.py
+++ djangoApi/api/neural/transform.py
@@ -26,7 +26,7 @@ def Bert(query: str) -> str:
 
 
     # Функция для выделения релевантных предложений
-    def extract_relevant_sentences(text, threshold=0):
+    def extract_relevant_sentences(text, threshold=0.5):
         sentences = text.split('. ')
         relevant_sentences = []
 
